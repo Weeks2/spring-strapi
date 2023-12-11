34aee219089b8ae668062644b440ced521e652d9
Christian Ibarra <chris.cjarne@gmail.com>
2023-12-11 00:17:07 -0600

    init spring

---
+++ spring/src/main/java/com/weeks2/strapi/StrapiApplication.java
@@ -0,0 +1,13 @@
+package com.weeks2.strapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrapiApplication.class, args);
+	}
+
+}
---
+++ spring/src/test/java/com/weeks2/strapi/StrapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weeks2.strapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
